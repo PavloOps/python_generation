d415a2524286a3866057501f95594dcdd96c606a
PavloOps <olga.lavrichenko@mail.ru>
2023-04-02 11:57:39 +0300

    add json task #2-4

--- working_with_files/json_training.py
+++ working_with_files/json_training.py
@@ -11,5 +11,59 @@ def task1():
     return result
 
 
+def task2():
+    words = {
+             frozenset(["tap", "telephone"]): ("tæp", "telifəun"),
+             "travel": "trævl",
+             ("hello", "world"): ("həˈləʊ", "wɜːld"),
+             "moonlight": "muːn.laɪt",
+             "sunshine": "ˈsʌn.ʃaɪn",
+             ("why", "is", "so", "difficult"): ("waɪ", "ɪz", "səʊ", "ˈdɪfɪkəlt"),
+             "adventure": "ədˈventʃər",
+             "beautiful": "ˈbjuːtɪfl",
+             frozenset(["spoon", "block"]): ("spu:n", "blɔk"),
+             "bicycle": "baisikl",
+             ("pilot", "fly"): ("pailət", "flai")
+            }
+    data_json = json.dumps(words, ensure_ascii=False, skipkeys=True)
+    return data_json
+
+
+def task3():
+    club1 = {"name": "FC Byern Munchen", "country": "Germany", "founded": 1900,
+             "trainer": "Julian Nagelsmann", "goalkeeper": "M. Neuer", "league_position": 1}
+
+    club2 = {"name": "FC Barcelona", "country": "Spain", "founded": 1899,
+             "trainer": "Xavier Creus", "goalkeeper": "M. Ter Stegen", "league_position": 7}
+
+    club3 = {"name": "FC Manchester United", "country": "England", "founded": 1878,
+             "trainer": "Michael Carrick", "goalkeeper": "D. De Gea", "league_position": 8}
+
+    with open("data.json", "w", encoding="utf-8") as json_file:
+        json.dump([club1, club2, club3], json_file, indent=3)
+
+    with open("data.json", encoding="utf-8") as json_file:
+        print(json.load(json_file))
+
+
+def task4():
+    specs = {
+             'Модель': 'AMD Ryzen 5 5600G',
+             'Год релиза': 2021,
+             'Сокет': 'AM4',
+             'Техпроцесс': '7 нм',
+             'Ядро': 'Cezanne',
+             'Объем кэша L2': '3 МБ',
+             'Объем кэша L3': '16 МБ',
+             'Базовая частота': '3900 МГц'
+            }
+
+    specs_json = json.dumps(specs, indent=3, ensure_ascii=False)
+    return specs_json
+
+
 if __name__ == "__main__":
     print(task1())
+    print(task2())
+    print(task3())
+    print(task4())
