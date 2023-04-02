a6b9a19fe8f532cd04baadda6b76f3277616244f
PavloOps <olga.lavrichenko@mail.ru>
2023-04-02 09:43:46 +0300

    csv for task 13

--- working_with_files/json_training.py
+++ working_with_files/json_training.py
@@ -1,3 +1,15 @@
+import json
+
+
+def task1():
+    countries = {'Monaco': 'Monaco', 'Iceland': 'Reykjavik', 'Kenya': 'Nairobi', 'Kazakhstan': 'Nur-Sultan',
+                 'Mali': 'Bamako', 'Colombia': 'Bogota', 'Finland': 'Helsinki', 'Costa Rica': 'San Jose',
+                 'Cuba': 'Havana', 'France': 'Paris', 'Gabon': 'Libreville', 'Liberia': 'Monrovia',
+                 'Angola': 'Luanda', 'India': 'New Delhi', 'Canada': 'Ottawa', 'Australia': 'Canberra'}
+
+    res = json.dumps(countries, separators=(',', ' - '), indent=3, sort_keys=True)
+    return res
+
+
 if __name__ == "__main__":
-    print("Let's start learning JSON!")
-    print("I'll try to do in next week...")
+    print(task1())
