44c949c32bbfd22dbfaa458b315855821108c5bc
PavloOps <olga.lavrichenko@mail.ru>
2023-04-28 11:21:15 +0300

    added tasks 1-2 for recursion

--- working_with_recursion.py
+++ working_with_recursion.py
@@ -1,3 +1,6 @@
+# from lecture: print(print_numbers(0, 10))
+
+
 def print_numbers(start, end):
     def rec(num):
         if num <= end:
@@ -6,4 +9,26 @@ def print_numbers(start, end):
     rec(start)
 
 
-print(print_numbers(0, 10))
+# task 1
+def traffic(n):
+    if n > 0:
+        print('Не парковаться')
+        traffic(n - 1)
+
+
+# task 2
+def print_hundred(start, end):
+    def rec(num):
+        if num <= end:
+            print(num)
+            rec(num + 1)
+
+    rec(start)
+
+
+def main():
+    print(print_hundred(1, 100))
+
+
+if __name__ == "main":
+    main()
