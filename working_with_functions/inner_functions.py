67b75f7afbce9880906c8df1231049a8d2d3bcd8
PavloOps <olga.lavrichenko@mail.ru>
2023-06-05 14:17:51 +0300

    added functions to part 5

--- working_with_functions/inner_functions.py
+++ working_with_functions/inner_functions.py
@@ -1,12 +1,13 @@
+from datetime import date
+from urllib.parse import urlencode
+
+
 def power(x):
     return lambda num: num**x
 
 
 def generator_square_polynom(a, b, c):
     return lambda x: a*x**2 + b*x + c
-
-
-from urllib.parse import urlencode
 
 
 def sourcetemplate(url):
@@ -17,3 +18,15 @@ def sourcetemplate(url):
         else:
             return url
     return combine
+
+
+def date_formatter(country_code):
+    def format_date(input_date):
+        d = {'ru': '%d.%m.%Y',
+             'us': '%m-%d-%Y',
+             'ca': '%Y-%m-%d',
+             'br': '%d/%m/%Y',
+             'fr': '%d.%m.%Y',
+             'pt': '%d-%m-%Y', }
+        return input_date.strftime(d[country_code])
+    return format_date
